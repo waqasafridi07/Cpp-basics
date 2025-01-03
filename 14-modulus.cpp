e7ccd974eccaf279c7b406c278f1ae87db3e8f2a
waqasafridi07 <waqaswc07@gmail.com>
2025-01-03 18:38:33 +0500

    i found modulus using different conditions

--- 14-modulus.cpp
+++ 14-modulus.cpp
@@ -12,7 +12,7 @@ int main(){
   //cout<<a%b;//if a%-b then answer is always normal (a%b)
   //int a = -10;
   //int b = 4;
-  //cout<<a%b;//if -a%b then the answer is always negative
+  //cout<<a%b;//if -a%b then the answer is always negative -(a%b)
   //int a = -10;
   // b = -4;
 ///cout<<a%b;//if -a%-b then also answer is always negative
