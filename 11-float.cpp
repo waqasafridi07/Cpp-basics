11d7c3f330b5cad43be28b624066c5ef81ca45af
waqasafridi07 <waqaswc07@gmail.com>
2025-01-03 17:43:43 +0500

    i printed outputs using float data type

---
+++ 11-float.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a = 2;
+    float b = 5;
+     cout<<a+b<<endl;
+     cout<<a-b<<endl;
+      cout<<a*b<<endl;
+       cout<<a/b<<endl;
+
+
+}
