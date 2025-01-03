d05adfcb14ed74c4bd67bc2020a460afe7fee786
waqasafridi07 <waqaswc07@gmail.com>
2025-01-03 18:03:00 +0500

    i found percentage using int data type

---
+++ 13-challenge.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // marks out of 30
+    int m = 21;
+    int u = 19;
+    int p = 27;
+    int i = 29;
+    int e = 17;
+    cout<<(m+u+p+i+e)/5;
+
+}
