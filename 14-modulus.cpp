21a92754193ca6bd2bf998dc53713ea00098827b
waqasafridi07 <waqaswc07@gmail.com>
2025-01-03 18:33:51 +0500

    i found modulus using different conditions

---
+++ 14-modulus.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = 4;
+  cout<<a%b;
+   // int a = 4;
+  //  int b = 10;
+  //  cout<<a%b;// if a<b and a and b are positive then answer is always equal to a
+  //int a = 10;
+  //int b = -4;
+  //cout<<a%b;//if a%-b then answer is always normal (a%b)
+  //int a = -10;
+  //int b = 4;
+  //cout<<a%b;//if -a%b then the answer is always negative
+  //int a = -10;
+  // b = -4;
+///cout<<a%b;//if -a%-b then also answer is always negative
+}
