566f25445911f943c1ae05989c8fcc79a05d995e
waqasafridi07 <waqaswc07@gmail.com>
2025-01-03 17:55:42 +0500

    i found percentage of 5 subjects using float data type

---
+++ 12-percentage.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // marks out of 100
+    float p = 73;
+    float m = 92;
+    float b = 81;
+    float c = 96;
+    float e = 76;
+    cout<<(p+m+b+c+e)/5;
+}
