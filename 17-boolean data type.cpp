b119dc1830d269d4a3ba49ca441db1741fcfc1a8
waqasafridi07 <waqaswc07@gmail.com>
2025-01-03 19:31:08 +0500

    i printed boolean data types

--- 17-boolean data type.cpp
+++ 17-boolean data type.cpp
@@ -3,4 +3,6 @@ using namespace std;
 int main(){
     bool x = true;
     cout<<x;//any non zero number is considered as true in boolean e.g 0.24224 or 122435
+    //bool x = false;
+    //cout<<x;//any zero number is considered as false in boolean e.g 00000 or 0.00000
 }
