1d7f9dc0a13562137257acecf83c5be3bbac8663
waqasafridi07 <waqaswc07@gmail.com>
2025-01-03 19:52:59 +0500

    i wrote a code on rules of naming variables

---
+++ 18-naming rules.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int _x1y = 6243;
+    cout<<_x1y;//number should start from an alphabet or some special characters like _ $ and numbers can be placed between alphabets or characters
+}
