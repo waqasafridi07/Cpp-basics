8d48508b0a04c96a8a6fa97669b3cc96c8c3b905
waqasafridi07 <waqaswc07@gmail.com>
2025-01-28 16:43:07 +0500

    finding ascii values or converting char into int or int into char

--- 29-Ascii values..cpp
+++ 29-Ascii values..cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-    char x = 'a';
-    int ascii = (int)x;
-    cout<<ascii; // imp ascii values of integer 'a'= 97; 'A'= 65; '0'= 48;
+    //char x = 'a';
+    //int ascii = (int)x;
+    //cout<<ascii; // imp ascii values of integer 'a'= 97; 'A'= 65; '0'= 48;
     // cout<<ch+0; // another way to print 
     // cout<<(int)x; // another way to print
+    int x = 97;
+    cout<<(char)x;
 }
