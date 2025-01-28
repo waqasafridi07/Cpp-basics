862b451c158c3fb6d922179fd1a5508e68e1b370
waqasafridi07 <waqaswc07@gmail.com>
2025-01-28 14:24:05 +0500

    Assignment operators

---
+++ 23-assignment operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int x = 10;
+    x += 5;
+    cout<<x;
+    //int x = 10;
+     //x -= 5;
+    //cout<<x;<< output is 5;
+    //int x = 10;
+    //x *= 5;
+    //cout<<x;<< output is 50;
+    //int x = 10;
+    //x /= 5;
+    //cout<<x;<< output is 2;
+
+}
