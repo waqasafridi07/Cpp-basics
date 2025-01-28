137d275f3b4ba3d51c9de3862380217c381c8b78
waqasafridi07 <waqaswc07@gmail.com>
2025-01-28 19:52:33 +0500

    changes added

--- 29-Ascii values 2.cpp
+++ 29-Ascii values 2.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-    //char x = 'a';
-    //int ascii = (int)x;
-    //cout<<ascii;
+    char x = 'a';
+    int ascii = (int)x;
+    cout<<ascii;
     // cout<<(int)x; // another way to print;
     // cout<<ch+0; // another way to print;
-    int x = 97;
-    cout<<(char)x;
+    //int x = 97;
+    //cout<<(char)x; // for finding reverse ascii value
 }
