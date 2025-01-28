4cca026497abb0ed98c7ab7100d940f277599f37
waqasafridi07 <waqaswc07@gmail.com>
2025-01-28 16:33:44 +0500

    finding ascii value or converting char into int

--- 29-Ascii values..cpp
+++ 29-Ascii values..cpp
@@ -4,4 +4,6 @@ int main(){
     char x = 'a';
     int ascii = (int)x;
     cout<<ascii; // imp ascii values of integer 'a'= 97; 'A'= 65; '0'= 48;
+    // cout<<ch+0; // another way to print 
+    // cout<<(int)x; // another way to print
 }
