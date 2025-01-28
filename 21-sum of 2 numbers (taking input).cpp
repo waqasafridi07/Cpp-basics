67ba6ea2c52628383c44505c9aa79a9d8abbe072
waqasafridi07 <waqaswc07@gmail.com>
2025-01-28 13:57:49 +0500

    sum of two numbers (taking input)

---
+++ 21-sum of 2 numbers (taking input).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter your first number : ";
+    cin>>x;
+    cout<<"Enter your second number : ";
+    cin>>y;
+    cout<<"Sum of numbers is : ";
+    cout<<x+y;
+}
