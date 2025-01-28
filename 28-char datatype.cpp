910258368d27921a38c67355cf80199e0b6495b4
waqasafridi07 <waqaswc07@gmail.com>
2025-01-28 15:48:33 +0500

    char data type

---
+++ 28-char datatype.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char x = '#';//char data type stores only a single letter or number or sign and consider it as a character
+    cout<<x;
+}
