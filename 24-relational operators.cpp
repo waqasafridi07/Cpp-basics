b76e315e9a4f0f03df9902a5f41d12fb3a85c6f3
waqasafridi07 <waqaswc07@gmail.com>
2025-01-28 14:45:49 +0500

    Relational operators

---
+++ 24-relational operators.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a = 5;
+    int b = 1;
+    cout<<(a==b);
+    //int a = 5;
+    //int b = 1;
+    //cout<<(a!=b);<< true;
+    //int a = 5;
+    //int b = 1;
+    //cout<<(a>b); true;
+    //int a = 5;
+    //int b = 1;
+    //cout<<(a<b); false;
+    //int a = 5;
+    //int b = 5;
+    //cout<<(a>=b);<< true;
+    //int a = 5;
+    //int b = 1;
+    //cout<<(a<=b);<< true;
+
+}
