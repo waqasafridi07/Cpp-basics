e1d3af3ae1177b1e901e4f5ae2cc28da634613d9
waqasafridi07 <waqaswc07@gmail.com>
2025-01-28 21:04:45 +0500

    challenge exercise

---
+++ 34-challenge.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter a number : ";
+    cin>>x;
+    if(x%5==0 && x%3==0){
+        cout<<"divisible";
+        }
+        else{
+            cout<<"Not divisible";
+        }
+    
+}
