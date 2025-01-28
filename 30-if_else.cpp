5943f2b803a3ebd0140c1d00b3ce409552a29387
waqasafridi07 <waqaswc07@gmail.com>
2025-01-28 19:48:45 +0500

    if.else

---
+++ 30-if_else.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter a Number : ";
+    cin>>x;
+    if(x%2==0){
+        cout<<"Even Number";
+    }
+    else{
+        cout<<"Odd number";
+    }
+}
